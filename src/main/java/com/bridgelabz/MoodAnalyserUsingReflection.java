fd6cca5005f0df5efc56ec18345b07cd6539cb77
kirtibhave <kirtibhave1000@gmail.com>
2020-03-29 13:01:41 +0530

    ADDED:Use reflection to create mood analyser using default constructor

---
+++ src/main/java/com/bridgelabz/MoodAnalyserUsingReflection.java
@@ -0,0 +1,35 @@
+package com.bridgelabz;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+public class MoodAnalyserUsingReflection {
+    //Reflection For Default Constructor
+    public static MoodAnalyser  createMoodAnalyzerDefault(){
+        try {
+            Constructor constructor = Class.forName("com.bridgelab.junit.MoodAnalyzer").getConstructor();
+            MoodAnalyser moodAnalyzer = (MoodAnalyser) constructor.newInstance();
+            return  moodAnalyzer;
+        } catch (ClassNotFoundException | NoSuchMethodException e ) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public  static  Constructor<?> getConstructor(String className,Class constructor) throws MoodAnalysisException {
+        try {
+            Class<?> moodAnalyzer = Class.forName(className);
+            return moodAnalyzer.getConstructor(constructor);
+        } catch (ClassNotFoundException e) {
+            throw new MoodAnalysisException(MoodAnalysisException.EnumExceptionType.CLASS_NOT_FOUND, e.getMessage());
+        } catch (NoSuchMethodException e) {
+            throw new MoodAnalysisException(MoodAnalysisException.EnumExceptionType.NO_SUCH_METHOD, e.getMessage());
+        }
+    }
+}
--- src/test/java/com/bridgelabz/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyserTest.java
@@ -3,6 +3,9 @@ package com.bridgelabz;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 public class MoodAnalyserTest {
 
     //1.1
@@ -67,4 +70,59 @@ public class MoodAnalyserTest {
             Assert.assertEquals(MoodAnalysisException.EnumExceptionType.NULL_MESSAGE, e.type);
         }
     }
+
+    //4.1 for default constructor
+    @Test
+    public void givenMoodAnalyserClass_WhenProper_ShouldReturnObject() {
+        try {
+            Constructor<?> constructor = Class.forName("com.bridgelabz.MoodAnalyser").getConstructor(String.class);
+            Object object = constructor.newInstance("I am in happy mood");
+            MoodAnalyser moodAnalyser = (MoodAnalyser) object;
+            String mood = moodAnalyser.analyseMood();
+            Assert.assertEquals("Happy", mood);
+        } catch (ClassNotFoundException | NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (MoodAnalysisException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void givenMoodAnalyserClass_EqualsMethod_toCheckTwoObjectsAreEqual() {
+        try {
+            MoodAnalyser moodAnalyzer = MoodAnalyserUsingReflection.createMoodAnalyzerDefault();
+            Assert.assertEquals(new MoodAnalyser(), moodAnalyzer);
+        } catch (MoodAnalysisException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Test
+    public void givenMoodAnalyserClass_When_ClassNameIsImproper() {
+        try {
+            Constructor<?> moodAnalyzer = new MoodAnalyserUsingReflection().getConstructor("com.bridgenbflabz.MoodAnalyser", String.class);
+
+        } catch (MoodAnalysisException e) {
+            Assert.assertEquals(MoodAnalysisException.EnumExceptionType.CLASS_NOT_FOUND, e.type);
+
+        }
+    }
+
+    @Test
+    public void givenMoodAnalyserClass_WhenConstructorIsNotProper(){
+        try {
+            Constructor<?> moodAnalyser = new MoodAnalyserUsingReflection().getConstructor("com.bridgelabz.MoodAnalyser", int.class);
+
+        } catch (MoodAnalysisException e) {
+            Assert.assertEquals(MoodAnalysisException.EnumExceptionType.NO_SUCH_METHOD, e.type);
+        }
+    }
 }
+
